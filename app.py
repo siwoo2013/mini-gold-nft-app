e6350a182c99e150a30666a6313820b7f9957447
siwoo2013 <min9999min@gmail.com>
2025-10-29 14:49:20 +0900

    서브페이지 상단만 통일

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from blueprints.phone import bp_phone
 
 def create_app():
@@ -11,7 +11,25 @@ def create_app():
 
     @app.route("/")
     def index():
-        return render_template("index.html")
+        # Serve the static landing page
+        return send_from_directory('.', 'index.html')
+
+
+    @app.route('/event')
+    def event():
+        return send_from_directory('.', 'event.html')
+
+    @app.route('/invest')
+    def invest():
+        return send_from_directory('.', 'invest.html')
+
+    @app.route('/service')
+    def service():
+        return send_from_directory('.', 'service.html')
+
+    @app.route('/my')
+    def my():
+        return send_from_directory('.', 'my.html')
 
     return app
 
