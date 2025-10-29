2a4593146955606f210b96fd78b2a192930b0d9f
siwoo2013 <min9999min@gmail.com>
2025-10-29 15:36:35 +0900

    remove: delete old public directory (serve from root)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 from blueprints.phone import bp_phone
 
 def create_app():
@@ -11,25 +11,7 @@ def create_app():
 
     @app.route("/")
     def index():
-        # Serve the static landing page
-        return send_from_directory('.', 'index.html')
-
-
-    @app.route('/event')
-    def event():
-        return send_from_directory('.', 'event.html')
-
-    @app.route('/invest')
-    def invest():
-        return send_from_directory('.', 'invest.html')
-
-    @app.route('/service')
-    def service():
-        return send_from_directory('.', 'service.html')
-
-    @app.route('/my')
-    def my():
-        return send_from_directory('.', 'my.html')
+        return render_template("index.html")
 
     return app
 
